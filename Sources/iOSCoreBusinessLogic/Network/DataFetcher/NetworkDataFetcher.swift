f9fc382771c58dd1c29490da3de794cd3d7a0c62
Mark Webb <Testablekarma@users.noreply.github.com>
2021-01-12 14:12:20 +0000

    Makes fetch public

--- Sources/iOSCoreBusinessLogic/Network/DataFetcher/NetworkDataFetcher.swift
+++ Sources/iOSCoreBusinessLogic/Network/DataFetcher/NetworkDataFetcher.swift
@@ -10,7 +10,7 @@ open class NetworkDataFetcher<T: Decodable>: DataFetcher<T, NetworkHttpService.E
         self.httpService = httpService
     }
 
-    override func fetch(request: URLRequest) -> AnyPublisher<T, NetworkHttpService.Errors>  {
+    public override func fetch(request: URLRequest) -> AnyPublisher<T, NetworkHttpService.Errors>  {
         httpService.performRequest(request)
             .decode(type: T.self, decoder: JSONDecoder())
             .mapError { self.errorTransformer.transform(error: $0) }
