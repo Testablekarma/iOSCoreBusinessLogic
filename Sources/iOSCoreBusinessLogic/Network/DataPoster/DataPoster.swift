58d3e61d681beaa0bab77800533e713166c06b68
Mark Webb <webbm5199@gmail.com>
2022-01-06 19:47:09 +0000

    Adds async await to dataposter

--- Sources/iOSCoreBusinessLogic/Network/DataPoster/DataPoster.swift
+++ Sources/iOSCoreBusinessLogic/Network/DataPoster/DataPoster.swift
@@ -5,4 +5,8 @@ open class DataPoster<T: Decodable, E: Encodable, U: Error> {
     func post(request: URLRequest, body: E) -> AnyPublisher<T, U> {
         fatalError("No logic should be implemented.")
     }
+    
+    func post(request: URLRequest, body: E) throws -> T {
+        fatalError("No logic should be implemented.")
+    }
 }
--- Sources/iOSCoreBusinessLogic/Network/DataPoster/NetworkDataPoster.swift
+++ Sources/iOSCoreBusinessLogic/Network/DataPoster/NetworkDataPoster.swift
@@ -5,6 +5,7 @@ open class NetworkDataPoster<T: Decodable, E: Encodable>: DataPoster<T, E, Netwo
     
     private let httpService: HttpService
     private lazy var errorTransformer = NetworkErrorTransformer()
+    private lazy var decoder = Decoder<T>()
     private let postingUrlRequestTransformer = JSONPostingURLRequestTransformer<E>()
 
     public init(httpService: HttpService) {
@@ -22,5 +23,36 @@ open class NetworkDataPoster<T: Decodable, E: Encodable>: DataPoster<T, E, Netwo
         } catch {
             return Result.Publisher(.encodingError).eraseToAnyPublisher()
         }
+    }
+    
+    public override func post(request: URLRequest, body: E) throws -> T {
+                
+        Task { () -> T in
+            let postRequest = try postingUrlRequestTransformer.transform(urlRequest: request, body: body)
+            return try await postAsync(request: postRequest)
+        } as! T
+    }
+
+}
+
+private extension NetworkDataPoster {
+    func postAsync(request: URLRequest) async throws -> T {
+        let task = Task { () -> T in
+            do {
+                let requestResult = try await httpService.performAsyncRequest(request)
+                
+                switch requestResult {
+                case let .success(data):
+                    return try decoder.decode(data: data)
+                    
+                case let .failure(error):
+                    throw error
+                }
+            } catch {
+                throw NetworkHttpService.Errors.unknown
+            }
+        }
+        
+        return try await task.value
     }
 }
